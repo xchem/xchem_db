e9833030dda11ba9ebec7145ef99c40df1da9183
Rachael Skyner <reskyner@googlemail.com>
2021-01-15 17:30:57 +0000

    update to write not append sql

--- run_sql_gen.py
+++ run_sql_gen.py
@@ -5,4 +5,4 @@ if os.path.isdir('xchem_db/migrations'):
     for f in glob.glob('xchem_db/migrations/*.py'):
         if '__init__' not in f:
             migration_name = f.split('/')[-1].replace('.py', '')
-            os.system(f'python manage.py sqlmigrate xchem_db {migration_name} >> sql/{migration_name}.sql')
+            os.system(f'python manage.py sqlmigrate xchem_db {migration_name} > sql/{migration_name}.sql')
