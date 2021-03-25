f4e15e220b8f3cdd73be677ef181f9caea36528b
Rachael Skyner <reskyner@googlemail.com>
2021-03-25 12:59:14 +0000

    Update compile_init.py

--- compile_init.py
+++ compile_init.py
@@ -5,5 +5,5 @@ for f in glob.glob('./sql/*.sql'):
     to_add = open(f, 'r').read()
     str_out += to_add
 
-with open('init.sql', 'w') as w:
+with open('init.sql', 'w+') as w:
     w.write(str_out)
