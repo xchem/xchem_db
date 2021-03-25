31c756521d4c239430b21c35eccc47394186df15
Rachael Skyner <reskyner@googlemail.com>
2021-03-25 12:11:13 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 # To use a consistent encoding
 from codecs import open
 from os import path
+from os import environ
 
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
@@ -15,7 +16,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version="0.3.0-beta",
+    version=environ.get('STACKMANAGER_VERSION'),
     description="Library for XChem Django database",
     long_description=long_description,
     # The project's main homepage.
