825db6bf4ec682d71c96b3174299c48e7fcced2b
Rachael Skyner <reskyner@googlemail.com>
2021-06-21 10:35:51 +0100

    remove ispyb_dja from import

--- xchem_db/models.py
+++ xchem_db/models.py
@@ -1,7 +1,7 @@
 from __future__ import unicode_literals
 from django.db import models
 
-from ispyb_dja.django_auth.models import IspybAuthorization
+from django_auth.models import IspybAuthorization
 
 #INVENTORY DATA
 
